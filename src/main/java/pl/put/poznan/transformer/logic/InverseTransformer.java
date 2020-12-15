ebb9422ca2db3f12f8806a4e025e3f8a4cad1a88
RoIIo <daniel5353@interia.eu>
2020-12-15 15:35:59 +0100

    powinno byc dobrze

--- src/main/java/pl/put/poznan/transformer/logic/InverseTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InverseTransformer.java
@@ -1,5 +1,5 @@
 package pl.put.poznan.transformer.logic;
-
+import java.lang.*;
 public class InverseTransformer extends TextTransformer{
 
     public InverseTransformer(String[] transforms) {
@@ -10,9 +10,42 @@ public class InverseTransformer extends TextTransformer{
         return super.transform(inverse(text));
     }
 
-    //Odwracanie ciągu znaków z zachowaniem wielkości liter
-    private String inverse(String text){
-        //TODO
-        return text;
+    /**
+     * Odwracanie ciągu znaków z zachowaniem wielkości liter
+     * @param text tekst do zamiany
+     * @return odwrocony tekst z zachowana kolejnosca duzych liter
+     */
+    public String inverse(String text){
+        boolean[] upper = new boolean[text.length()];
+
+        for (int i =0; i<text.length();i++)
+        {
+            if(text.charAt(i)>='A' && text.charAt(i)<= 'Z')
+            {
+                upper[i] = true;
+            }
+            else
+            {
+                upper[i] = false;
+            }
+        }
+        String result = "";
+        for(int i = text.length()-1; i>=0;i--)
+        {
+            result += text.charAt(i);
+        }
+        String result1 = "";
+        for(int i=0 ; i < text.length();i++)
+        {
+            if(upper[i]== true)
+            {
+                result1 += Character.toUpperCase(result.charAt(i));
+            }
+            else
+            {
+                result1+= Character.toLowerCase(result.charAt(i));
+            }
+        }
+        return result1;
     }
 }
