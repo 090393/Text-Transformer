68e92001e91f47b82004fb5a66c8828503ef39bc
RoIIo <daniel5353@interia.eu>
2020-12-15 12:16:44 +0100

    Cos na poczatek

--- src/main/java/pl/put/poznan/transformer/logic/IntegerTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/IntegerTransformer.java
@@ -10,7 +10,22 @@ public class IntegerTransformer extends NumbersTransformer {
 
     //Przekształcanie liczb całkowitych na słowa
     private String transformInteger(String text){
-        //TODO
-        return text;
+        String result = "";
+        String[] hundreds = {"sto ", "dwiescie ","trzysta ","czterysta ","piecset ","szescset ","siedemset ","osiemset ","dziewiecset "};
+        String[] dozen = {"", "dziesiec ","dwadziescia ","trzydziesci ","czterdziesci ","piecdziesiat ","szescdziesiat ","siedemdziesiat ","osiemdziesiat ","dziewiecdziesiat "};
+        String[] unity = {"","jeden","dwa","trzy","cztery","piec","szesc","siedem","osiem","dziewiec"};
+        if( text.length()==3)
+        {
+            result = hundreds[text.charAt(0)-48] + dozen[text.charAt(1)-48] + unity[text.charAt(2)-48];
+        }
+        if( text.length()==2)
+        {
+            result = dozen[text.charAt(0)-48] + unity[text.charAt(1)-48];
+        }
+        if( text.length()==1)
+        {
+            result = unity[text.charAt(0)-48];
+        }
+        return result;
     }
 }
