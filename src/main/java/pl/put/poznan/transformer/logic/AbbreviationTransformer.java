0cdfc3352d7fca7a20d45880eb7808896e0dfeff
Natalia Czyżewska <50886301+090393@users.noreply.github.com>
2020-12-15 16:13:38 +0100

    Update AbbreviationTransformer.java

--- src/main/java/pl/put/poznan/transformer/logic/AbbreviationTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/AbbreviationTransformer.java
@@ -7,8 +7,22 @@ public class AbbreviationTransformer extends TextTransformer {
     public String transform(String text){ return abbreviate(text); }
 
     //Zamiana słów na skróty
+
+    /**Zamiana słów na skróty
+     *
+     * @param text tekst wejściowy
+     * @return teskt z zamienionymi słowami na stróty
+     */
     private String abbreviate(String text){
         //TODO
+        text = text.replace("na przykład", "np.");
+        text = text.replace("NA PRZYKŁAD", "NP.");
+        text = text.replace("między innymi", "m.in.");
+        text = text.replace("MIĘDZY INNYMI", "M.IN.");
+        text = text.replace("i tym podobne", "itp.");
+        text = text.replace("I TYM PODOBNE", "ITP.");
+        text = text.replace("i tak dalej", "itd.");
+        text = text.replace("I TAK DALEJ", "ITD.");
         return text;
     }
 }
