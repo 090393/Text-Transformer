3b8b0df118e678ca2fd3d4de6862e9467cb3a570
Natalia Czyżewska <50886301+090393@users.noreply.github.com>
2020-12-21 10:33:49 +0100

    Update LowerTransformer.java

--- src/main/java/pl/put/poznan/transformer/logic/LowerTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/LowerTransformer.java
@@ -1,23 +1,23 @@
 package pl.put.poznan.transformer.logic;
+
 /**
- * Klasa słuzaca do zamiany liter na małe.
+ * Klasa służąca do zamiany liter na małe.
  * @author Krzychu
  */
-public class LowerTransformer extends TextTransformer {
+public class LowerTransformer extends TextTransformerDecorator {
 
-    public LowerTransformer(String[] transforms) {
-        super(transforms);
+    public LowerTransformer(Transformer transformer) {
+        super(transformer);
     }
 
-    public String transform(String text){ return lower(text); }
+    public String transform(String text){ return super.transform(lower(text)); }
 
-    /** Zammiana liter na małe
+    /** Zamiana liter na małe
      *
      * @param text Tekst wejściowy
      * @return Wynikowy tekst
      */
-
-    private String lower(String text){
+    public String lower(String text){
         return text.toLowerCase();
     }
 }
