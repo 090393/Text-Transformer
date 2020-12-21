4c36d023fa8d3fcf68192972ac158a93c8520edf
Natalia Czyżewska <50886301+090393@users.noreply.github.com>
2020-12-21 10:51:19 +0100

    Update IntegerTransformer.java

--- src/main/java/pl/put/poznan/transformer/logic/IntegerTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/IntegerTransformer.java
@@ -1,58 +1,111 @@
 package pl.put.poznan.transformer.logic;
-import java.lang.*;
 
 /**
- * Klasa służąca do zamiany zdania wejściowego w taki sposób,
- * aby na wyjściu pojawiło się to samo zdanie w odwróconej kolejności
- * i z zachowaniem wielkości liter na danym miejscu.
+ * Klasa służąca do przekształcania zdania wejściowego w taki sposób,
+ * aby na wyjściu zamiast liczb pojawił się zapis tych liczb w słowach.
  * @author Rollo
  */
-public class InverseTransformer extends TextTransformerDecorator{
+public class IntegerTransformer extends TextTransformerDecorator {
 
-    public InverseTransformer(Transformer transformer) {
+    public IntegerTransformer(Transformer transformer) {
         super(transformer);
     }
 
     public String transform(String text){
-        return super.transform(inverse(text));
+        return super.transfrom(transformInteger(text));
     }
 
     /**
-     * Metoda służąca do odwracania ciągu znaków z zachowaniem wielkości liter
-     * @param text tekst do zamiany
-     * @return odwrócony tekst z zachowaniem kolejności wielkich liter
+     * Metoda służąca do wyszukiwania liczb w zdaniu
+     * @param text zdanie do przekształcenia
+     * @return zwraca zdanie z przekształconymi liczbami na słowa
      */
-    public String inverse(String text){
-        boolean[] upper = new boolean[text.length()];
-
-        for (int i =0; i<text.length();i++)
-        {
-            if(text.charAt(i)>='A' && text.charAt(i)<= 'Z')
-            {
-                upper[i] = true;
-            }
-            else
-            {
-                upper[i] = false;
-            }
-        }
+    public String transformInteger(String text){
+        String tmp = "";
         String result = "";
-        for(int i = text.length()-1; i>=0;i--)
+        boolean number = false;
+        for( int i =0; i<text.length();i++)
         {
-            result += text.charAt(i);
-        }
-        String result1 = "";
-        for(int i=0 ; i < text.length();i++)
-        {
-            if(upper[i]== true)
+            if (text.charAt(i)>=48 && text.charAt(i) <=57)
             {
-                result1 += Character.toUpperCase(result.charAt(i));
+                tmp += text.charAt(i);
+                number = true;
             }
             else
             {
-                result1+= Character.toLowerCase(result.charAt(i));
+                number = false;
+            }
+            if (!tmp.isEmpty() && !number)
+            {
+                result += transform_number(tmp);
+                tmp = "";
+            }
+            if (!number)
+            {
+                result += text.charAt(i);
             }
         }
-        return result1;
+        if (!tmp.isEmpty() && number) {
+            result += transform_number(tmp);
+            tmp = "";
+        }
+        return result;
+    }
+
+    /**
+     * Metoda służąca do zamieniania liczby na słowa
+     * @param text liczba na wejście
+     * @return zamieniona liczba na słowa
+     */
+    public String transform_number(String text)
+    {
+        int erase=0, len = text.length();
+        String tmp = text;
+        for(int i = 0 ; i<len-1;i++)
+        {
+            if(text.charAt(i) == 48)
+            {
+                erase = i+1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        text = tmp.substring(erase,len);
+        if (text.length() > 3) return text;
+        String result = "";
+        String[] hundreds = {"sto ", "dwieście ", "trzysta ","czterysta ","pięćset ", "sześćset ","siedemset ","osiemset ","dziewięćset "};
+        String[] teen = {"jedenaście","dwanaście","trzynaście","czternaście","piętnaście","szesnaście","siedemnaście","osiemnaście","dziewiętnaście"};
+        String[] dozen = {"", "dziesięć ","dwadzieścia ","trzydzieści ","czterdzieści ","pięśdziesiąt ","sześćdziesiąt ","siedemdziesiąt ","osiemdziesiąt ","dziewiećdziesiąt "};
+        String[] unity = {"","jeden","dwa","trzy","cztery","pięć","sześć","siedem","osiem","dziewięć"};
+        if ( text.length() == 3 )
+        {
+            result = hundreds[text.charAt(0)-49] + dozen[text.charAt(1)-48] + unity[text.charAt(2)-48];
+            if(text.charAt(1) == 49 && text.charAt(2)>48)
+            {
+                result = hundreds[text.charAt(1)-49] + teen[text.charAt(2)-49];
+            }
+        }
+        if ( text.length() == 2 )
+        {
+            result = dozen[text.charAt(0) - 48] + unity[text.charAt(1) - 48];
+            if (text.charAt(0) == 49 && text.charAt(1) > 48 )
+            {
+                result = teen[text.charAt(1)-49];
+            }
+        }
+        if ( text.length() == 1 )
+        {
+            result = unity[text.charAt(0)-48];
+            if(text.charAt(0)==48) result = "zero";
+        }
+        int x = result.length()-1;
+        if ( result.charAt(x) == 32 )
+        {
+
+            result = result.substring(0, x);
+        }
+        return result;
     }
 }
